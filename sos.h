dae08d9de2c217202d5d1f407ba899123c930f13
MinaRaoufAziz <i.minaraouf@gmail.com>
2019-10-23 15:08:11 +0200

    Create sos.h

---
+++ sos.h
@@ -0,0 +1,42 @@
+/*
+ * sos.h
+ *
+ *  Created on: Oct 22, 2019
+ *      Author: Mina
+ */
+
+#ifndef SOS_H_
+#define SOS_H_
+
+#include "timer.h"
+#include <stdlib.h>
+#define MAX_TASKS 	5
+
+typedef enum {OVF_TASKS_BUFFER_OS = -9, NULLPTR_OS, OK_OS = 1}OS_Tasks_Status_t;
+
+typedef struct
+{
+	uint8 Timer_ID;
+	uint8 Systick_Time;
+}OS_Congif_t;
+typedef ((*Func_Ptr_t)(void));
+
+typedef enum {CREATED,READY, WAIT, RUN, DELETE}State_t;
+typedef struct
+{
+	Func_Ptr_t Task_Name; // function name
+	uint16 Periodicity;
+	uint8 Priority;
+	State_t Func_State;
+	uint16 Deadline;
+}OS_Task_t;
+
+OS_Task_t Tasks_Stack [MAX_TASKS];
+
+void SOS_Init(const OS_Congif_t * ConfigPtr);
+void SOS_Run(void);
+OS_Tasks_Status_t SOS_Create_Task (Func_Ptr_t Task_Name, uint16 Periodicity, uint8 Priority);
+void SOS_Delete_Task (Func_Ptr_t Task_Name);
+
+
+#endif /* SOS_H_ */
